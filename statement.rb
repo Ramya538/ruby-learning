698d200bdfb21adccc52d22604b873ae4ba881ce
Ramya538 <ramyaraj538@gmail.com>
2015-09-23 17:00:26 +0530

    add sample for calculating statement

---
+++ statement.rb
@@ -0,0 +1,12 @@
+END {
+puts "statement ends";
+}
+BEGIN {
+puts "simple if..else conditional statement";
+}
+i=2
+if i>2  
+puts "the value is {#i}"
+else
+puts "the value is not in between 0 to2"
+end
